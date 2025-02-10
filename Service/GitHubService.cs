48f051468100766bff26ccd5516890e1ed686c9c
Frayman <Frayman@DESKTOP-4U42C65>
2025-02-10 14:53:46 +0200

    Add project files.

---
+++ Service/GitHubService.cs
@@ -0,0 +1,42 @@
+﻿using Octokit;
+using static Service.GitHubService;
+
+namespace Service;
+
+public class GitHubService : IGitHubService
+{ 
+    private readonly GitHubClient _client;
+
+    public GitHubService()
+    {
+            _client =new GitHubClient(new ProductHeaderValue("my-cool-app"));
+
+    }
+
+
+public async Task<int> GetUserFollowersAsync(string userName)
+
+        {
+
+            var user = await _client.User.Get(userName);
+
+            return user.Followers;
+
+        }
+
+
+public async Task<List<Repository>> SearchRepositoriesInCSharp()
+
+        {
+
+            var request = new SearchRepositoriesRequest("repo-name") { Language =  Language.CSharp };
+
+            var result = await _client.Search.SearchRepo(request);
+
+            return result.Items.ToList();
+
+        }
+
+    }
+
+
